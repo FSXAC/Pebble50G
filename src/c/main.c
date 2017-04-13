bcfbfa71bbe9a6f81ad65c5ff3d1922f58de533a
Muchen He <mansurhe@outlook.com>
2017-04-13 14:42:03 -0700

    Time now displays properly

--- src/c/main.c
+++ src/c/main.c
@@ -1,4 +1,4 @@
-#include <pebble.h>
+//#include <pebble.h>
 #include "main.h"
 
 // window global variable
@@ -19,8 +19,8 @@ static void main_window_load(Window *w) {
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
     text_layer_set_text(s_time_layer, "--:--");
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_26_BOLD_NUMBERS_AM_PM));
-    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_09));
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
     
     // add as child layer to main window
     layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -30,6 +30,25 @@ static void main_window_unload(Window *w) {
     // destroy elements in window
     // destroy textlayer
     text_layer_destroy(s_time_layer);
+}
+
+// subscribe to a function to tell the time
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
+
+// updates the text with time
+static void update_time() {
+    // get a tm structure
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+    
+    // write current hours and minutes to a buffer
+    static char s_buffer[8];
+    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    
+    // display the time to textlayer
+    text_layer_set_text(s_time_layer, s_buffer);
 }
 
 static void init() {
@@ -44,6 +63,12 @@ static void init() {
     
     // Show the Window on the watch, with animated=true
     window_stack_push(s_main_window, true);
+    
+    // Register with TickTimerService
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    
+    // make sure the time is dispayed from the start
+    update_time();
 }
 
 static void deinit() {
--- src/c/main.h
+++ src/c/main.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <pebble.h>
+
+static void main_window_load(Window *w);
+static void main_window_unload(Window *w);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
+static void update_time();
+static void init();
+static void deinit();
